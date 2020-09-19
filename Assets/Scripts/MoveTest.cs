d09410d8fe1b6dc33a197b1200d32359527abe7b
Seth Tal <seth.tal255@gmail.com>
2020-09-19 00:03:36 -0700

    More Project Setup

---
+++ Assets/Scripts/MoveTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTest : MonoBehaviour
+{
+    public Rigidbody2D _rb = null;
+    public float speed = 0f;
+    public Vector2 directionVector
+    {
+        get
+        {
+            Vector2 result = Vector2.zero;
+
+            result.x = Input.GetAxis("Horizontal");
+            result.y = Input.GetAxis("Vertical");
+
+            return result;
+        }
+    }
+
+    void Start() {}
+    void Update()
+    {
+        if (_rb != null)
+        {
+            _rb.AddForce(directionVector * speed * Time.deltaTime, ForceMode2D.Force);
+        }
+    }
+}
